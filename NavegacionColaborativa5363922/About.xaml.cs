d316d958ad05630c35d935933cca802f25ccd5df
Admin <5834157@clases.edu.sv>
2024-07-01 15:34:45 -0600

    Agregé el algoritmo dinosario donde hará la conversión de peso y alturaa
    tambie  se agregó una sección de nosotros donde está toda la información de nuestro grupo

--- NavegacionColaborativa5363922/About.xaml.cs
+++ NavegacionColaborativa5363922/About.xaml.cs
@@ -1,9 +1,29 @@
 namespace NavegacionColaborativa5363922;
-
+/// <summary>
+/// <createddate> 01/07/2024 </createddate> 
+///<company>INSTITUTO NACIONAL CANTON LOURDES</company>
+///<lastmodificationdate> 01/07/2024 </lastmodificationdate>
+///<lastmodificationdescription> Se coloca el codigo que dara funcion a la app</lastmodificationdescription>
+///<lastmodifierautor> Yony David Rodríguez Chávez </lastmodifierautor>
+/// </summary>
 public partial class About : ContentPage
 {
+    /// <summary>
+    /// Se crea la navegacion a la siguiente pagina
+    /// </summary>
 	public About()
 	{
 		InitializeComponent();
 	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private async void Button_Clicked_1(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new MainPage());
+
+    }
 }
