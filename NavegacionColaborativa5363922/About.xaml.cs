47f069767338105390a9ead90a2501b9a5819d1e
Admin <5834157@clases.edu.sv>
2024-07-01 15:51:37 -0600

    Realize el algoritmo de dinosaurio, diseñando la interfaz de la aplicación y el código para que funcione.
    Tambien realize la pagina de About que resume un poco acerca del equipo

--- NavegacionColaborativa5363922/About.xaml.cs
+++ NavegacionColaborativa5363922/About.xaml.cs
@@ -24,6 +24,5 @@ public partial class About : ContentPage
     private async void Button_Clicked_1(object sender, EventArgs e)
     {
         await Navigation.PushAsync(new MainPage());
-
     }
 }
