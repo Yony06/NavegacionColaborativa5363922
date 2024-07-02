8b1b9c97371a52c2253878ed9d5adb3bacacf2b2
Daniel <6657336@clases.edu.sv>
2024-07-02 13:52:18 -0600

    Cree una clase llamada Literals donde declare la variable que contendra la lista de equipos. En el codigo cs de SurveyetailsView, añadimos el codigo para que muestre la lista de equipos por medio de un boton

---
+++ NavegacionColaborativa5363922/Literals.cs
@@ -0,0 +1,19 @@
+﻿namespace NavegacionColaborativa5363922
+{
+
+    /// <summary>
+    ///<createddate> 02/07/2024 </createddate>
+    ///<company> INSTITUTO NACIONAL CANTON LOURDES</company>
+    ///<lastmodificationdate> 02/07/2024 </lastmodificationdate>
+    ///<lastmodificationdescription> Hacemos public la clase</lastmodificationdescription>
+    ///<lastmodifierautor> Daniel Steven López Hernández </lastmodifierautor>
+    /// </summary>
+
+    public class Literals
+    {
+        /// <summary>
+        /// Declaramos la variable
+        /// </summary>
+        public const string FavoriteTEamTitle ="Seleccion de equipo";
+    }
+}
--- NavegacionColaborativa5363922/SurveyDetailsView.xaml.cs
+++ NavegacionColaborativa5363922/SurveyDetailsView.xaml.cs
@@ -1,15 +1,32 @@
+
 namespace NavegacionColaborativa5363922;
-/// <summary>
-/// <createddate> 30/06/2024 </createddate> 
-///<company>INSTITUTO NACIONAL CANTON LOURDES</company>
-///<lastmodificationdate> 30/06/2024 </lastmodificationdate>
-///<lastmodificationdescription> Se coloca el coddigo que dara funcion a la app</lastmodificationdescription>
-///<lastmodifierautor> Andrea Rocio Pérez Arriola </lastmodifierautor>
-/// </summary>
+	/// <summary>
+	/// <createddate> 02/07/2024 </createddate> 
+	///<company>INSTITUTO NACIONAL CANTON LOURDES</company>
+	///<lastmodificationdate> 02/07/2024 </lastmodificationdate>
+	///<lastmodificationdescription> Se coloca el coddigo que dara funcion a la app</lastmodificationdescription>
+	///<lastmodifierautor> Andrea Rocio Pérez Arriola </lastmodifierautor>
+	/// </summary>
 public partial class SurveyDetailsView : ContentPage
 {
+
 	/// <summary>
-	/// Colocamos el evento del botón
+	/// Lista de opciones de equipos que se le mostrara al usuario 
+	/// </summary>
+	private readonly string[] teams =
+	{
+		"Alianza Lima",
+		"Amercia",
+		"Boca Juniors",
+		"Caracas FC",
+		"Colo-Colo",
+		"Perañol",
+		"Real Madrid",
+		"Saprissa"
+	};
+
+	/// <summary>
+	/// 
 	/// </summary>
 	public SurveyDetailsView()
 	{
@@ -18,6 +35,19 @@ public partial class SurveyDetailsView : ContentPage
 
     private void Button_Clicked(object sender, EventArgs e)
     {
+    }
 
+    /// <summary>
+    /// Mostrara un boton donde al tocarlo nos mostrara un listado con los 
+	/// equipos y se ocultara temporalmente el boton al selecionar un equipo
+	/// se mostrara en otro label, ocultara la lista y se volvera a mostrar el boton
+    /// </summary>
+    private async void Button_Clicked_1(object sender, EventArgs e)
+    {
+		var favoriteTeam = await DisplayActionSheet(Literals.FavoriteTEamTitle,null,null,teams);
+		if (!string .IsNullOrWhiteSpace(favoriteTeam))
+		{
+            FavoriteTeamLabel.Text = favoriteTeam;
+        }
     }
 }
